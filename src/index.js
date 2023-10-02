0f0bff34ba43a4ed2c7647e3b92c60095c6d7aa9
einsteinkibet <einsteinkibet0110@gmail.com>
2023-10-02 07:11:57 +0300

    comleted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document
+  .getElementById("create-task-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    
+  });
